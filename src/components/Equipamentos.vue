c018d1685dc9640a6d9c66fbe99a7be379fc6288
Matheus de Souza <matheusdesouza187@gmail.com>
2023-01-14 18:13:18 -0300

    feat: implementação da nova interface do componente de equipamentos

--- src/components/Equipamentos.vue
+++ src/components/Equipamentos.vue
@@ -1,23 +1,24 @@
 <template>
-    <div>
+    <div class="border py-4 px-3 rounded-3 bg-danger shadow-lg">
         <div class="row">
             <div class="col">
-                <h5><i class="bi-bag me-2"></i>Equipamentos</h5>
-                <ul class="nav nav-tabs">
+                <h5 class="text-white"><i class="bi-bag me-2"></i>Equipamentos</h5>
+                <ul class="nav nav-tabs border-0">
                     <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'equipamentos', params: { tipo: 'carros' } }">Carros</router-link>
+                        <router-link class="nav-link text-white" :to="{ name: 'equipamentos', params: { tipo: 'carros' } }">Carros</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'equipamentos', params: { tipo: 'telefones' } }">Telefones</router-link>
+                        <router-link class="nav-link text-white" :to="{ name: 'equipamentos', params: { tipo: 'telefones' } }">Telefones</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'equipamentos', params: { tipo: 'kits-de-reanimacao' } }">Kits de Reanimação</router-link>
+                        <router-link class="nav-link text-white" :to="{ name: 'equipamentos', params: { tipo: 'kits-de-reanimacao' } }">Kits de Reanimação</router-link>
                     </li>
                 </ul>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
+        <hr class="text-white">
+        <div class="row mt-3">
+            <div class="col-11 mx-auto bg-white py-3 px-4 rounded-3">
                 <span><router-view name="equipamentos" /></span>
             </div>
         </div>
@@ -29,3 +30,9 @@ export default {
     name: "Equipamentos"
 }
 </script>
+
+<style scoped>
+.router-link-exact-active {
+    font-weight: bold;
+}
+</style>
