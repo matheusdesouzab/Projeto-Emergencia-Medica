73e257006ce329824a3a8c05834b61ddc013c382
Matheus de Souza <matheusdesouza187@gmail.com>
2023-01-14 12:00:46 -0300

    feat: criação dos componentes: Equipe e ListaEquipes, e modificações no componente de Equipes para se adaptar as novas implementações

---
+++ src/components/ListaEquipes.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Equipe></Equipe>
+    </div>
+</template>
+
+<script>
+import Equipe from '@/components/Equipe'
+
+export default {
+    name: 'ListaEquipes',
+    components: {
+        Equipe
+    }
+}
+</script>
